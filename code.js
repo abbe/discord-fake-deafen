e8fbfc111491589e26f167477a11dfc65c8ec043
itsmeboy0 <75471378+itsmeboy0@users.noreply.github.com>
2021-04-01 18:31:14 +0800

    Fix user not able to move or disconnect to a channel

--- code.js
+++ code.js
@@ -1,12 +1,22 @@
 var text = new TextDecoder("utf-8");
+var old_channel_id;
 
 WebSocket.prototype.original = WebSocket.prototype.send;
 WebSocket.prototype.send = function(data) {
     if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
-        if (text.decode(data).includes("self_deaf")) {
-            console.log("found mute/deafen");
+        var textData = text.decode(data);
+        console.log(textData);
+        var channel_id = textData.substring(
+            textData.lastIndexOf('channel_id') + 15, 
+            textData.lastIndexOf('channel_id') + 33
+        );
+        console.log(channel_id);
+        if (old_channel_id === channel_id) {
+            console.log("Found mute/deafen");
             data = data.replace('"self_mute":false', 'NiceOneDiscord');
-            console.log("faked - borkgang.com");
+            console.log("Faked mute/deafen");
+        } else {
+            old_channel_id = channel_id;
         }
     }
     WebSocket.prototype.original.apply(this, [data]);
