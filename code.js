252c74296a53927244e64c7b87fd5bedb7bf1071
Hook <83718844+HookPub@users.noreply.github.com>
2021-07-06 13:16:41 -0500

    Trimmed off the unneeded code.

--- code.js
+++ code.js
@@ -1,12 +1,11 @@
+  
 var text = new TextDecoder("utf-8");
 
 WebSocket.prototype.original = WebSocket.prototype.send;
 WebSocket.prototype.send = function(data) {
     if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
         if (text.decode(data).includes("self_deaf")) {
-            console.log("found mute/deafen");
-            data = data.replace('"self_mute":false', 'NiceOneDiscord');
-            console.log("faked - borkgang.com");
+            data = data.replace('"self_mute":false',);
         }
     }
     WebSocket.prototype.original.apply(this, [data]);
