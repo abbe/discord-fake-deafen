d0fdb519f578bb2ad451f25b628edf576a5ca53e
HookPub <hookpublishing@gmail.com>
2021-07-18 14:52:30 -0500

    new just mute script -H

---
+++ fakemute.js
@@ -0,0 +1,11 @@
+var text = new TextDecoder("utf-8");
+
+WebSocket.prototype.original = WebSocket.prototype.send;
+WebSocket.prototype.send = function(data) {
+    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+        if (text.decode(data).includes("self_mute")) {
+            data = data.replace('"self_mute":false',);
+        }
+    }
+    WebSocket.prototype.original.apply(this, [data]);
+}
